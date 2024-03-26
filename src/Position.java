75bb234ba45f402dc017487a5d2b68c9028d12d6
Pickles888 <verysaltypickles@gmail.com>
2024-03-25 18:04:51 -0700

    bean

---
+++ src/Position.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Position {
+    private int x;
+    private int y;
+    
+    public Position(String position) {
+
+    }
+
+    public int getX() {
+
+    }
+
+    public int getY() {
+
+    }
+}
